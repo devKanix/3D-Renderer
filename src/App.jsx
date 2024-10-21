f5ab1c5680befef1f0f02262d83df1ef43e0c0a9
Kanishk Chaudhary <opkanix@gmail.com>
2024-10-21 12:09:04 +0530

    "Removed import and usage of SPLoader component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Topics, Hero, Navbar, Tech, StarsCanvas } from './components';
-import SPLoader from "./components/SpinnerLoader";
+// import SPLoader from "./components/SpinnerLoader";
 
 const App=() => {
   return (
     <BrowserRouter>
     <div>
-    <SPLoader />
+    {/* <SPLoader /> */}
     </div>
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
