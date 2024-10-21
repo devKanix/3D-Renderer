415c053cbafa04b1e5d5745d7f4acc29f7113111
Kanishk Chaudhary <opkanix@gmail.com>
2024-10-21 12:42:08 +0530

    "Removed SpinnerLoader component and isLoading state, reorganized App component structure"

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
-import { useState, useEffect } from 'react';
 import { About, Contact, Topics, Hero, Navbar, Tech, StarsCanvas } from './components';
-import SpinnerLoader from "./components/SpinnerLoader";
+// import SPLoader from "./components/SpinnerLoader";
 
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => setIsLoading(false);
-    window.addEventListener('load', handleLoad);
-
-    return () => window.removeEventListener('load', handleLoad);
-  }, []);
-
+const App=() => {
   return (
     <BrowserRouter>
-      {isLoading ? (
-        <SpinnerLoader text="Loading..." />
-      ) : (
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Topics />
-          <Tech />
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
-        </div>
-      )}
+    <div>
+    {/* <SPLoader /> */}
+    </div>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Topics/>
+      <Tech/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
